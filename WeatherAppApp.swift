5932acee13e04bec18552ae170c148edf0d432e8
President Raindas <presidentraindas@gmail.com>
2021-07-19 00:43:02 +0100

    Initial commit

---
+++ WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by President Raindas on 19/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
