88139027fb713004204b184763dc762e5de2a9e8
President Raindas <presidentraindas@gmail.com>
2021-07-19 19:25:20 +0100

    UI changes

--- SavedCitiesView.swift
+++ SavedCitiesView.swift
@@ -11,11 +11,38 @@ struct SavedCitiesView: View {
     
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
+    @State var searchQuery = ""
+    
     var body: some View {
-        Text("Saved Cities View")
-        Button("Dismiss"){
-            presentationMode.wrappedValue.dismiss()
-        }
+        VStack {
+            
+            HStack {
+                Text("Saved Cities").font(.largeTitle.bold())
+                Spacer()
+                Button(action: {presentationMode.wrappedValue.dismiss()}, label: {
+                    Image(systemName: "xmark").font(.largeTitle).foregroundColor(.primary)
+                })
+            }
+            
+            SearchBar(text: $searchQuery).onChange(of: searchQuery, perform: { query in
+                // function to perform query search
+            }).padding(.top)
+            
+            ScrollView {
+                ForEach(1..<4) { _ in
+                    HStack {
+                        Text("Lagos, Nigeria")
+                        Spacer()
+                        Image(systemName: "trash")
+                    }.font(.title2)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10.0)
+                    .padding(.vertical, 5.0)
+                }
+            }.padding(.top)
+            
+        }.padding()
     }
 }
 
